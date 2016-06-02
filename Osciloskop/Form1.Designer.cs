d07abf56cf72f410c73319fc4657458ed4a115a8
Branimir <rickobrnimir@gmail.com>
2016-06-03 01:02:19 +0200

    Now it is in English

--- Osciloskop/Form1.Designer.cs
+++ Osciloskop/Form1.Designer.cs
@@ -43,11 +43,12 @@
             // zoomBar
             // 
             this.zoomBar.LargeChange = 50;
-            this.zoomBar.Location = new System.Drawing.Point(12, 102);
+            this.zoomBar.Location = new System.Drawing.Point(9, 83);
+            this.zoomBar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.zoomBar.Maximum = 20;
             this.zoomBar.Minimum = 2;
             this.zoomBar.Name = "zoomBar";
-            this.zoomBar.Size = new System.Drawing.Size(617, 56);
+            this.zoomBar.Size = new System.Drawing.Size(463, 45);
             this.zoomBar.SmallChange = 10;
             this.zoomBar.TabIndex = 10;
             this.zoomBar.Value = 5;
@@ -55,9 +56,10 @@
             // 
             // connButton
             // 
-            this.connButton.Location = new System.Drawing.Point(13, 9);
+            this.connButton.Location = new System.Drawing.Point(10, 7);
+            this.connButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.connButton.Name = "connButton";
-            this.connButton.Size = new System.Drawing.Size(75, 27);
+            this.connButton.Size = new System.Drawing.Size(56, 22);
             this.connButton.TabIndex = 11;
             this.connButton.Text = "Connect";
             this.connButton.UseVisualStyleBackColor = true;
@@ -65,9 +67,10 @@
             // 
             // startButton
             // 
-            this.startButton.Location = new System.Drawing.Point(94, 9);
+            this.startButton.Location = new System.Drawing.Point(70, 7);
+            this.startButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.startButton.Name = "startButton";
-            this.startButton.Size = new System.Drawing.Size(75, 26);
+            this.startButton.Size = new System.Drawing.Size(56, 21);
             this.startButton.TabIndex = 12;
             this.startButton.Text = "Start";
             this.startButton.UseVisualStyleBackColor = true;
@@ -75,9 +78,10 @@
             // 
             // DisconnectButton
             // 
-            this.DisconnectButton.Location = new System.Drawing.Point(175, 9);
+            this.DisconnectButton.Location = new System.Drawing.Point(131, 7);
+            this.DisconnectButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.DisconnectButton.Name = "DisconnectButton";
-            this.DisconnectButton.Size = new System.Drawing.Size(92, 27);
+            this.DisconnectButton.Size = new System.Drawing.Size(69, 22);
             this.DisconnectButton.TabIndex = 13;
             this.DisconnectButton.Text = "Disconnect";
             this.DisconnectButton.UseVisualStyleBackColor = true;
@@ -86,35 +90,39 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 59);
+            this.label1.Location = new System.Drawing.Point(10, 48);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(183, 17);
+            this.label1.Size = new System.Drawing.Size(118, 13);
             this.label1.TabIndex = 14;
-            this.label1.Text = "Period osvježavanja ( ms ) :";
+            this.label1.Text = "Refresh interval  ( ms ) :";
             // 
             // RefreshRateTextBox
             // 
-            this.RefreshRateTextBox.Location = new System.Drawing.Point(202, 56);
+            this.RefreshRateTextBox.Location = new System.Drawing.Point(152, 46);
+            this.RefreshRateTextBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.RefreshRateTextBox.Name = "RefreshRateTextBox";
-            this.RefreshRateTextBox.Size = new System.Drawing.Size(65, 22);
+            this.RefreshRateTextBox.Size = new System.Drawing.Size(50, 20);
             this.RefreshRateTextBox.TabIndex = 15;
             this.RefreshRateTextBox.Text = "10";
             // 
             // Send
             // 
-            this.Send.Location = new System.Drawing.Point(273, 56);
+            this.Send.Location = new System.Drawing.Point(205, 32);
+            this.Send.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Send.Name = "Send";
-            this.Send.Size = new System.Drawing.Size(75, 23);
+            this.Send.Size = new System.Drawing.Size(56, 47);
             this.Send.TabIndex = 16;
-            this.Send.Text = "Popravi";
+            this.Send.Text = "Change";
             this.Send.UseVisualStyleBackColor = true;
             this.Send.Click += new System.EventHandler(this.Send_Click);
             // 
             // RestartEspButton
             // 
-            this.RestartEspButton.Location = new System.Drawing.Point(273, 8);
+            this.RestartEspButton.Location = new System.Drawing.Point(205, 6);
+            this.RestartEspButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.RestartEspButton.Name = "RestartEspButton";
-            this.RestartEspButton.Size = new System.Drawing.Size(92, 27);
+            this.RestartEspButton.Size = new System.Drawing.Size(69, 22);
             this.RestartEspButton.TabIndex = 17;
             this.RestartEspButton.Text = "Restart";
             this.RestartEspButton.UseVisualStyleBackColor = true;
@@ -125,18 +133,19 @@
             this.osci.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.osci.Location = new System.Drawing.Point(12, 164);
+            this.osci.Location = new System.Drawing.Point(9, 133);
+            this.osci.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.osci.Name = "osci";
-            this.osci.Size = new System.Drawing.Size(789, 294);
+            this.osci.Size = new System.Drawing.Size(592, 239);
             this.osci.TabIndex = 0;
             this.osci.Text = "grafPrikazOsciloskopa1";
             this.osci.zoom = 50;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(813, 470);
+            this.ClientSize = new System.Drawing.Size(610, 382);
             this.Controls.Add(this.RestartEspButton);
             this.Controls.Add(this.Send);
             this.Controls.Add(this.RefreshRateTextBox);
@@ -146,6 +155,7 @@
             this.Controls.Add(this.connButton);
             this.Controls.Add(this.zoomBar);
             this.Controls.Add(this.osci);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.zoomBar)).EndInit();
